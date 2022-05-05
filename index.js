5ab8bd5c880720dee2f5a6cf80eb654824422461
rehanmohiuddin <rehan.4942@gmail.com>
2022-05-05 22:16:44 +0530

    Fix:removed vercel

--- index.js
+++ index.js
@@ -38,6 +38,6 @@ app.use("/quiz", QuizRoute);
 
 app.get("/", (req, res) => res.send("QUIZ API"));
 
-server.listen(process.env.PORT || 3000, () => {
+server.listen(process.env.PORT, () => {
   console.log("Server Started At ", process.env.PORT);
 });
